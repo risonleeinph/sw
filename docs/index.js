6063031d8baa85a2d386ef111f4c16a625dad900
risonleeinph <l3496620@gmail.com>
2020-08-22 18:55:50 +0800

    修改资源路径

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
 // 注册service worker
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then(function (registration) {
         console.log('service worker register success!')
       })
